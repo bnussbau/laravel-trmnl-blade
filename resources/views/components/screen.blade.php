1e65dc67375db3b92287c3d92bc8301ccd8eea99
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-06-06 21:13:13 +0200

    chore: docs, readme

--- resources/views/components/screen.blade.php
+++ resources/views/components/screen.blade.php
@@ -6,8 +6,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Inter:300,400,500" rel="stylesheet"/>
     <link rel="stylesheet"
-          href="https://usetrmnl.com/css/{{ config('trmnl-blade.framework_version', 'latest') }}/plugins.css">
-    <script src="https://usetrmnl.com/js/{{ config('trmnl-blade.framework_version', 'latest') }}/plugins.js"></script>
+          href="https://usetrmnl.com/css/{{ config('trmnl-blade.framework_version', '1.1.0') }}/plugins.css">
+    <script src="https://usetrmnl.com/js/{{ config('trmnl-blade.framework_version', '1.1.0') }}/plugins.js"></script>
     <title>{{ $title ?? config('app.name') }}</title>
 </head>
 <body class="environment trmnl">
